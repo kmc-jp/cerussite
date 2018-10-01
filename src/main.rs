0ef23d3c76e86f9e161912da5d77c4a8e7a10864
statiolake <statiolake@gmail.com>
2018-10-01 22:30:01 +0900

    Support reading a value from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("define i32 @main() #0 {{\n  ret i32 {}\n}}", 42);
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = env::args().nth(1).expect("no file name supplied.");
+
+    let mut source = String::new();
+    File::open(&*file_name)?.read_to_string(&mut source)?;
+
+    let num: i32 = source.trim().parse()?;
+
+    println!("define i32 @main() #0 {{");
+    println!("  ret i32 {}", num);
+    println!("}}");
+
+    Ok(())
 }
