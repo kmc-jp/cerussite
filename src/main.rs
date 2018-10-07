0add65fd13ae1c40df64c40c5b9a3a9008829aba
Hideyuki Teramura <hide4d51@gmail.com>
2018-10-08 00:37:34 +0900

    Add add operator support

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut source = String::new();
     File::open(&*file_name)?.read_to_string(&mut source)?;
 
-    let num: i32 = source.trim().parse()?;
+    let operands: Vec<&str> = source.split("+").collect();
+    let mut res: i32 = 0;
+    // Check if the source file consists of only an empty expression.
+    // if so, generate LLVM IR that does nothing (returns 0).
+    if operands.len() == 1 && operands[0].trim().is_empty() {
+        res = 0;
+    } else {
+        let mut operands_parsed: Vec<i32> = Vec::new();
+        for o in operands {
+            let o = o.trim();
+            operands_parsed.push(o.parse()?);
+        }
+        for op in operands_parsed {
+            res += op;
+        }
+    }
 
     println!("define i32 @main() #0 {{");
-    println!("  ret i32 {}", num);
+    println!("  ret i32 {}", res);
     println!("}}");
 
     Ok(())
