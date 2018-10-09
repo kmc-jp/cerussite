5302c6df5bfd8aec20553091e62b87c6efe87348
statiolake <statiolake@gmail.com>
2018-10-09 18:19:18 +0900

    [cargo-test-cerussite] Support running test tool

--- test/cargo-test-cerussite/src/main.rs
+++ test/cargo-test-cerussite/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io;
 use std::process::Command;
 
@@ -7,6 +8,17 @@ fn main() -> io::Result<()> {
         .arg("build")
         .arg("--release")
         .current_dir("test/cerussite-test-tool")
+        .spawn()?
+        .wait()?;
+
+    let mut args = env::args();
+
+    assert_eq!(args.next().map(|x| x.contains("cargo")), Some(true));
+    assert_eq!(args.next(), Some("test-cerussite".into()));
+
+    // run test tool
+    Command::new("test/cerussite-test-tool/target/release/cerussite-test-tool")
+        .args(args)
         .spawn()?
         .wait()?;
 
