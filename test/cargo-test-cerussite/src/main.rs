64e1760da6b757d98b28f0ac52f956fde8146e82
statiolake <statiolake@gmail.com>
2018-10-09 18:08:57 +0900

    [cargo-test-cerussite] Support building test tool

--- test/cargo-test-cerussite/src/main.rs
+++ test/cargo-test-cerussite/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    // build test tool
+    Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .current_dir("test/cerussite-test-tool")
+        .spawn()?
+        .wait()?;
+
+    Ok(())
 }
