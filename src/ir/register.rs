76fb9b6d0bc5c9333e50e8c3a3ad48c022e3f097
hatsusato <knightspm5@gmail.com>
2018-10-14 12:21:22 +0900

    Impl Copy for Identity

--- src/ir/register.rs
+++ src/ir/register.rs
@@ -1,5 +1,12 @@
 struct Identity(i32);
+impl Clone for Identity {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+impl Copy for Identity {}
 #[test]
 fn test_identity() {
     let _a = Identity(0);
+    let _b = _a;
 }
