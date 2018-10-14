8f896fc7c97329a5e9115e83f528e66298508b01
hatsusato <knightspm5@gmail.com>
2018-10-14 22:36:19 +0900

    Impl make_add_instruction

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -20,13 +20,17 @@ enum Instruction<'a> {
 fn make_ret_instruction(val: Value) -> Instruction {
     Instruction::Ret(val)
 }
+fn make_add_instruction<'a>(gen: &'a mut IdentityGenerator,
+                            lhs: Value<'a>, rhs: Value<'a>) -> Instruction<'a> {
+    let reg = Register::new(gen);
+    Instruction::Add(reg, lhs, rhs)
+}
 #[test]
 fn test_instruction() {
     let mut gen = IdentityGenerator::new();
-    let reg = Register::new(&mut gen);
     let val1 = Value::Constant(1);
     let val2 = Value::Constant(2);
     let val3 = Value::Constant(3);
-    let _add = Instruction::Add(reg, val1, val2);
+    let _add = make_add_instruction(&mut gen, val1, val2);
     let _ret = make_ret_instruction(val3);
 }
