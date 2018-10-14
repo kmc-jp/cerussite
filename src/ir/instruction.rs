0524a5b742ce04e3adf4b8f83c6482ab924dc23f
hatsusato <knightspm5@gmail.com>
2018-10-14 21:55:12 +0900

    Introduce lifetime parameter

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -1,21 +1,21 @@
 use super::register::IdentityGenerator;
 use super::register::Register;
 
-enum Value {
+enum Value<'a> {
     Constant(i32),
-    Register(Register),
+    Register(&'a Register),
 }
 #[test]
 fn test_value() {
     let mut a = IdentityGenerator::new();
     let b = Register::new(&mut a);
     let _c = Value::Constant(0);
-    let _d = Value::Register(b);
+    let _d = Value::Register(&b);
 }
 
-enum Instruction {
-    Ret(Value),
-    Add(Register, Value, Value),
+enum Instruction<'a> {
+    Ret(Value<'a>),
+    Add(Register, Value<'a>, Value<'a>),
 }
 #[test]
 fn test_instruction() {
@@ -23,8 +23,8 @@ fn test_instruction() {
     let reg1 = Register::new(&mut gen);
     let reg2 = Register::new(&mut gen);
     let reg3 = Register::new(&mut gen);
-    let val1 = Value::Register(reg1);
-    let val2 = Value::Register(reg2);
+    let val1 = Value::Register(&reg1);
+    let val2 = Value::Register(&reg2);
     let val3 = Value::Constant(0);
     let _ret = Instruction::Ret(val1);
     let _add = Instruction::Add(reg3, val2, val3);
