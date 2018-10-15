29be4d872b3af333b470701dc1d0a52031c041d4
hatsusato <knightspm5@gmail.com>
2018-10-15 20:50:55 +0900

    Move definition of make_ret_instruction

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -17,8 +17,10 @@ enum Instruction<'a> {
     Ret(Value<'a>),
     Add(Register, Value<'a>, Value<'a>),
 }
-fn make_ret_instruction(val: Value) -> Instruction {
-    Instruction::Ret(val)
+impl<'a> Instruction<'a> {
+    fn ret(val: Value) -> Instruction {
+        Instruction::Ret(val)
+    }
 }
 fn make_add_instruction<'a>(gen: &'a mut IdentityGenerator,
                             lhs: Value<'a>, rhs: Value<'a>) -> Instruction<'a> {
@@ -38,5 +40,5 @@ fn test_instruction() {
     let val2 = Value::Constant(2);
     let add = make_add_instruction(&mut gen, val1, val2);
     let val3 = get_target(&add).unwrap();
-    let _ret = make_ret_instruction(val3);
+    let _ret = Instruction::ret(val3);
 }
