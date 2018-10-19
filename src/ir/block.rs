bcca085ea267d9e020895b4a26c49cb7fe0dbf86
hatsusato <knightspm5@gmail.com>
2018-10-20 01:56:36 +0900

    Impl push for BasicBlock

--- src/ir/block.rs
+++ src/ir/block.rs
@@ -8,15 +8,22 @@ impl BasicBlock {
         let vec = Vec::new();
         BasicBlock(reg, vec)
     }
+    fn push(&mut self, inst: Instruction) {
+        self.1.push(inst)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::super::value::*;
 
     #[test]
     fn test_basic_block() {
         let reg = Register::new();
-        let _bb = BasicBlock::new(reg);
+        let mut bb = BasicBlock::new(reg);
+        let val = Value::Constant(0);
+        let inst = Instruction::Ret(val);
+        bb.push(inst);
     }
 }
