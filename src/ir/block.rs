a53bcee90f3b8a37003d7a6f9dba59d08c594389
hatsusato <knightspm5@gmail.com>
2018-10-20 10:58:29 +0900

    Impl label for BasicBlock

--- src/ir/block.rs
+++ src/ir/block.rs
@@ -1,6 +1,7 @@
 use std::vec::Vec;
 use super::instruction::Instruction;
 use super::register::Register;
+use super::value::Value;
 
 pub struct BasicBlock(Register, Vec<Instruction>);
 impl BasicBlock {
@@ -11,12 +12,15 @@ impl BasicBlock {
     pub fn push(&mut self, inst: Instruction) {
         self.1.push(inst)
     }
+    pub fn label(&self) -> Value {
+        let reg = self.0.clone();
+        Value::Label(reg)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use super::super::value::*;
 
     #[test]
     fn test_basic_block() {
@@ -24,6 +28,7 @@ mod tests {
         let mut bb = BasicBlock::new(reg);
         let val = Value::Constant(0);
         let inst = Instruction::Ret(val);
+        let _label = bb.label();
         bb.push(inst);
     }
 }
