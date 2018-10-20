d423481d63c7f1dcd62e9790c18a533d8fb2bcc4
hatsusato <knightspm5@gmail.com>
2018-10-20 11:14:45 +0900

    Impl push for Function

--- src/ir/function.rs
+++ src/ir/function.rs
@@ -8,14 +8,21 @@ impl Function {
         let vec = Vec::new();
         Function(name, vec)
     }
+    fn push(&mut self, block: BasicBlock) {
+        self.1.push(block)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::super::register::Register;
 
     #[test]
     fn test_function() {
-        let _func = Function::new();
+        let mut func = Function::new();
+        let reg = Register::new();
+        let block = BasicBlock::new(reg);
+        func.push(block);
     }
 }
