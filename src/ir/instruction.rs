32e50b55cf27a3339290ca370ac1892212957bff
hatsusato <knightspm5@gmail.com>
2018-10-20 16:40:55 +0900

    Impl fmt for Instruction

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use super::value::Register;
 use super::value::Value;
 
@@ -10,6 +11,16 @@ impl Instruction {
         match self {
             Instruction::Ret(_) => init,
             Instruction::Add(reg, _, _) => reg.set(init),
+        }
+    }
+}
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Instruction::Ret(val) =>
+                write!(f, "ret i32 {}", val),
+            Instruction::Add(reg, lhs, rhs) =>
+                write!(f, "{} = add i32 {}, {}", reg, lhs, rhs),
         }
     }
 }
@@ -30,7 +41,12 @@ mod tests {
         let add = Instruction::Add(reg, val1, val2);
         let ret = Instruction::Ret(val3);
         let mut init = 0;
+        init = reg1.set(init);
+        init = reg2.set(init);
+        init = reg3.set(init);
         init = add.numbering(init);
         let _end = ret.numbering(init);
+        println!("{}", add);
+        println!("{}", ret);
     }
 }
